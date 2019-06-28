33106600c1fce7467e32ef0b0b38fb6c699e8f94
SBcodework <50241115+SBcodework@users.noreply.github.com>
2019-06-28 16:29:30 -0400

    Update common.cpp
    Added spaces for readability.

--- common.cpp
+++ common.cpp
@@ -11,7 +11,7 @@ void disp(int grid[100])
     int* ptr = grid;
     for (int i=0; i<100; i++, ptr++)
     {
-        if (i%10 == 0) {std::cout << "\n";}
+        if ( i % 10 == 0 ) { std::cout << "\n"; }
         std::cout << *ptr;
     }
     std::cout << "\n";
@@ -23,42 +23,42 @@ int** shipgen(int* grid, int length) // Grid is not supposed to change.
 
     {
         int start = (rand() % 100);
-        if (grid[start] == 3) {continue;}
-        int directions[4] {1,-1,10,-10}; // RIGHT, LEFT, DOWN, UP : used to verify directions. 0 means invalid
+        if ( grid[start] == 3 ) { continue; }
+        int directions[4] {1, -1, 10, -10}; // RIGHT, LEFT, DOWN, UP : used to verify directions. 0 means invalid
         int nvalid = 4;
-        for (int d=0, offset, index, xdigit=start / 10, l; d<4; d++) // Go through directions, checking each
+        for (int d = 0, offset, index, xdigit = start / 10, l; d < 4; d++) // Go through directions, checking each
         {
             offset = directions[d];
-            for (l=0; l<length; l++) // For (length) times
+            for (l = 0; l<length; l++) // For (length) times
             {
-                index = start + (l*offset);
-                if ( ((d<2)&&(index/10!=xdigit)) || ((index<0)||(index>99))
-                    || (grid[index]==3)) // Check if out of range, then if filled
+                index = start + (l * offset);
+                if ( ( (d < 2) && (index / 10 != xdigit) ) || ((index<0)||(index>99))
+                    || (grid[index] == 3) ) // Check if out of range, then if filled
                 {
-                    directions[d]=0;
+                    directions[d] = 0;
                     nvalid--;
                     break;
                 }
             }
         }
-        if (nvalid==0) {continue;}
+        if (nvalid == 0) {continue;}
         // By this point, function will return within this iteration of the for loop
         int chosendir;
-        if (nvalid==1) {chosendir = 0;}
-        else {chosendir = (rand() % nvalid);}
+        if (nvalid == 1) {chosendir = 0;}
+        else { chosendir = (rand() % nvalid); }
         // Find the index of the chosendir
         int dirIndex;
-        for (int a=0; a<4; a++)
+        for (int a = 0; a < 4; a++)
         {
-            if (directions[a]==0) {continue;}
-            if (chosendir == 0) {dirIndex = a; break;}
+            if ( directions[a] == 0 ) { continue; }
+            if ( chosendir == 0 ) { dirIndex = a; break; }
             chosendir--;
         }
         int** ship = new int*[length];
         int offset = directions[dirIndex];
         for (int i=0, index; i<length; i++)
         {
-            index = start + (i*offset);
+            index = start + (i * offset);
             ship[i] = &(grid[index]);
         }
         return ship;
@@ -68,7 +68,7 @@ int** shipgen(int* grid, int length) // Grid is not supposed to change.
 
 void gridplace(int** ship, int len)
 {
-    for(int i=0; i<len; i++)
+    for(int i = 0; i < len; i++)
     {
         *(ship[i]) = 3;
     }
